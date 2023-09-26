93344dce8b8c938aa4afcd523adb50b1ee3fd914
myllymaj <myllymaj@arcada.fi>
2023-09-26 18:26:05 +0300

    cleanup - no updates

--- endpoints/boards.js
+++ endpoints/boards.js
@@ -1,18 +1,44 @@
 const express = require('express')
-const routeManager = express.Router()
-const { PrismaClient } = require('@prisma/client')
+const router = express.Router()
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
 require('dotenv').config()
 
 
+router.post('/boards', async (req, res) => {
+  const { title, ownerId } = req.body; // Destructure the title and ownerId from the request body
 
+  try {
+    // Check if the user exists (you didn't provide the code for this, so I'm assuming you have a way to check)
+    if (!userExists(ownerId)) {
+      return res.status(404).json({ message: 'Användaren hittades inte' });
+    }
 
+    const newBoard = await prisma.board.create({
+      data: {
+        title,
+        ownerId,
+      },
+    });
 
-routeManager.get('/boards', async (req, res) => {
+    res.status(201).json(newBoard); // Respond with the newly created board
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ msg: 'ERROR', error: 'Internal server error' });
+  }
+});
+
+router.get('/boards', async (req, res) => {
     const userId = req.user.userId;
   
     try {
      
-        const availableBoards = await Board.find({ ownerId: userId });
+      const availableBoards = await prisma.board.findMany({
+        where: { ownerId: userId },
+      });
+      
+
         res.json(availableBoards);
       } catch (error) {
         console.error(error);
@@ -20,4 +46,4 @@ routeManager.get('/boards', async (req, res) => {
 
       }
     });
-module.exports = routeManager
+    module.exports = router;
--- public/login/script.js
+++ public/login/script.js
@@ -1,44 +1,4 @@
 
-function setCookie(name, value, days) {
-  const expirationDate = new Date();
-  expirationDate.setDate(expirationDate.getDate() + days);
-  const cookieValue = encodeURIComponent(value) + "; expires=" + expirationDate.toUTCString() + "; path=/";
-  document.cookie = name + "=" + cookieValue;
-}
-
-
-function getCookie(name) {
-  const cookieName = name + "=";
-  const cookies = document.cookie.split(';');
-  for (let i = 0; i < cookies.length; i++) {
-    let cookie = cookies[i];
-    while (cookie.charAt(0) === ' ') {
-      cookie = cookie.substring(1);
-    }
-    if (cookie.indexOf(cookieName) === 0) {
-      return decodeURIComponent(cookie.substring(cookieName.length, cookie.length));
-    }
-  }
-  return null;
-}
-
-
-
-function isLoggedIn() {
-    return !!localStorage.getItem('jwtToken') || !!getCookie('jwtToken');
-  }
-
-  if (isLoggedIn()) {
-    console.log('User is logged in.');
-    fetchUserBoards(); // 
-  } else {
-    console.log('User is not logged in.');
-  }
-  
-
-
-
-
 
 
 //login
@@ -61,90 +21,33 @@ updateDisplay();
 */
 
     
-    const apiUrl = 'http://localhost:3030/users/login';
+const apiUrl = 'http://localhost:3030/users/login';
 
-    try {
-        const response = await fetch(apiUrl, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ email, password }),
-        });
+try {
+    const response = await fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+    });
 
-        const data = await response.json();
+    const data = await response.json();
 
-        if (response.ok) {
-      
-            const jwtTok = data.token;
-            console.log('JWT token:', data.token);
-            localStorage.setItem('jwtToken',jwtTok);
-            setCookie('jwtToken', jwtTok,1);
-            console.log('ID:', data.userId);
-          
-            const tokenDisplay = document.getElementById('tokenDisplay');
-          
-            console.log(localStorage.getItem('jwtToken'))
-            //tokenDisplay.textContent = `JWT token: ${data.token}`;
-            
-           
-            fetchUserBoards();
-          } else {
-            console.error('Error:', data.msg);
-          }
-    } catch (error) {
-        console.error('Error:', error);
+    if (response.ok) {
+        console.log('Inloggad');
+        console.log('JWT token:', data.token);
+        console.log('ID:', data.userId);
+
+        const tokenDisplay = document.getElementById('tokenDisplay');
+        tokenDisplay.textContent = `JWT token: ${data.token}`;
+    } else {
+        console.error('Error:', data.msg);
     }
+} catch (error) {
+    console.error('Error:', error);
+}
 
 
 
 });
-
-//hämta boards om successful login
-async function fetchUserBoards() {
-console.log("hämta in boards")
-    const jwtToken = localStorage.getItem('jwtToken');
-  
-    if (!jwtToken) {
-      console.error('JWT token is missing.');
-      return;
-    }
-  
-    const apiUrl = 'http://localhost:3030/boards'; 
-  
-    try {
-      const response = await fetch(apiUrl, {
-        method: 'GET',
-        headers: {
-          'Authorization': jwtToken,
-        },
-      });
-  
-      const data = await response.json();
-  
-      if (response.ok) {
-        
-        const boardsList = document.getElementById('boardsList');
-        boardsList.innerHTML = ''; 
-  
-        if (data.length === 0) {
-          boardsList.textContent = 'Ingen Virtual Board hittades';
-        } else {
-          data.forEach((board) => {
-            const boardItem = document.createElement('li');
-            boardItem.textContent = board.name; 
-            boardsList.appendChild(boardItem);
-          });
-        }
-  
-    
-        document.getElementById('userBoards').style.display = 'block';
-      } else {
-        console.error('Error:', data.msg);
-      }
-    } catch (error) {
-      console.error('Error:', error);
-    }
-  }
-  
-
